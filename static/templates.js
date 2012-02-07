97ddc036a30dd2db7e76497fb436e243f2f88f10
Marek <marekz@gmail.com>
2012-02-06 23:57:28 -0500

    init

---
+++ static/templates.js
@@ -0,0 +1,10 @@
+var templates = {};
+
+templates.layout = '<div id="question"></div><div id="entry"></div><div class="clear"></div><div id<div id="numpad"></div><div class="clear"></div><div id="score"></div>';
+
+templates.question = '<span class="number">{{operand1}}</span> {{operator}} <span class="number">{{operand2}}</span></div> =';
+
+templates.intro = '<div id="intro"><h1>Edubotics</h1><p>Learning with robot friends</p></div>';
+templates.victory = '<div id="victory"><h1>Great job.</h1><p>Keep up the good work!</p></div>';
+
+//templates.score = '<div id="score"></div>'
